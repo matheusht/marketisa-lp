19a61653e1e927e8c0f16c3e52de73d0589baca6
matheusht <dev.matheustheodoro@gmail.com>
2024-05-31 16:00:17 -0300

    Home Component Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import { Button } from "./components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
 export default function Home() {
   return (
-    <main>
-    homepage
-    </main>
-
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span>Marketing</span>
+            <h1>
+              Hello Im <br /> <span>Marketisa</span>
+            </h1>
+          </div>
+          <div>
+            {/* photo */}
+            <div>photo</div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
