6a5d964e9ffb6110c3dbb1932d1e497cbe65e3e2
matheusht <dev.matheustheodoro@gmail.com>
2024-06-03 17:23:36 -0300

    services page finished

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,7 +1,181 @@
+"use client";
+
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+} from "react-icons/fa";
+
+const about = {
+  title: "About me",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Marketisa",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+44) 99830-6069 ",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "+12 Years",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Brazillian",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "isabelathedoromarketing@gmail.com",
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available",
+    },
+  ],
+};
+
+const experience = {
+  icon: "/assets/resume/badge.svg",
+  title: "My Experience",
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  items: [
+    {
+      company: "Integrado",
+      position: "Marketing",
+      duration: "2022 - 2024",
+    },
+    {
+      company: "Marketisa",
+      position: "Marketing Manager",
+      duration: "2024 - Present",
+    },
+  ],
+};
+
+const education = {
+  icon: "/assets/resume/cap.svg",
+  title: "My education",
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  items: [
+    {
+      institution: "Unicesumar",
+      degree: "Marketing",
+      duration: "2016 - 2020",
+    },
+    {
+      institution: "Trafego Pago",
+      degree: "Advertising",
+      duration: "2024 - Present",
+    },
+  ],
+};
+
+const skills = {
+  title: "My skills",
+  description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "HTML 5",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "CSS 3",
+    },
+    {
+      icon: <FaFigma />,
+      name: "Figma",
+    },
+    {
+      icon: <FaJs />,
+      name: "Javascript",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "NodeJS",
+    },
+    {
+      icon: <FaReact />,
+      name: "ReactJS",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "NextJS",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "TailwindCSS",
+    },
+  ],
+};
+
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../components/ui/tabs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../components/ui/tooltip";
+
+import { ScrollArea } from "../components/ui/scroll-area";
+import { animate, motion } from "framer-motion";
+
 export default function Resume() {
   return (
-    <div>
-      resume page
-    </div>
-  )
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col xl:flew-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About Me</TabsTrigger>
+          </TabsList>
+          <div className="min-h-[70vh] w-full">
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+              </div>
+            </TabsContent>
+
+            <TabsContent value="education" className="w-full">
+              education
+            </TabsContent>
+            <TabsContent value="skills" className="w-full">
+              skills
+            </TabsContent>
+            <TabsContent value="about" className="w-full">
+              about
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 }
