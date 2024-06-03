ab9cf90cbda819a1c582feffa736c84bf468accd
matheusht <dev.matheustheodoro@gmail.com>
2024-06-03 15:26:06 -0300

    Image Component

--- src/app/components/Photo.tsx
+++ src/app/components/Photo.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export function Photo() {
   return (
     <div className="w-full h-full relative">
-      <motion.div className="">
+      <motion.div initial={{ opacity: 0 }}>
         <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
           <Image
             src="/assets/photo.png"
