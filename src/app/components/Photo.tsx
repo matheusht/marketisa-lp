d47483cd472f43e65cf105ea2ff46f934b3383e7
matheusht <dev.matheustheodoro@gmail.com>
2024-06-24 17:28:56 -0300

    marketisa Main img

--- src/app/components/Photo.tsx
+++ src/app/components/Photo.tsx
@@ -18,10 +18,10 @@ export function Photo() {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] absolute"
         >
           <Image
-            src="/assets/photo.png"
+            src="/assets/Marketisa.svg"
             priority
             quality={100}
             fill
